417be584522e19f9286ddb7e8b56a4b06086f26c
omot-atihsamay <ymsttomo.401510@gmail.com>
2026-08-28 14:35:35 +0900

    feat:各種Scene作成&遷移ボタン作成

--- Assets/Script/Scenes/ScreenManager.cs
+++ Assets/Script/Scenes/ScreenManager.cs
@@ -10,6 +10,11 @@ public class ScreenManager : MonoBehaviour
     {
         SampleScene,
         testScene,
+        Title,
+        Config,
+        Result,
+        ResultList,
+        StageSelect,
     }
 
     private void Awake()
